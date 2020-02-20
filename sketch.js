75317db070695877de37cc6a3b6c6ae04651b0a2
JeeyaTalati <2235.stkabirnrnp@gmail.com>
2020-02-20 18:48:47 +0530

    DATABASESgit add sketch.jsgit add sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,13 @@
 var ball;
-
+var database;
+var position;
 function setup(){
+    database=firebase.database();
     createCanvas(500,500);
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+    var ballPositionRef = database.ref('ball/position');
+    ballPositionRef.on("value",readPosition);
 }
 
 function draw(){
@@ -24,6 +28,16 @@ function draw(){
 }
 
 function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+    database.ref('ball/position').set({
+        'x':position.x + x,
+        'y': position.y +y
+    })
+
+     
+}
+
+function readPosition(data){
+    position = data.val();
+    ball.x = position.x;
+    ball.y = position.y;
 }
